def5872b225a83daddf43c1e5515edd71a80d8ef
wither3 <84785651+wither3@users.noreply.github.com>
2024-11-27 17:29:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ./index.js
 const express = require('express');
 const cors = require('cors');
-const { downloadTikTok } = require('./codenya/ttdownload'); // Pastikan path ini sesuai dengan lokasi file ytdownload.js
+const { downloadTikTok } = require('./codenya/ttdownload'); // Pastikan path ini sesuai dengan lokasi file ttdownload.js
 
 const app = express();
 
@@ -15,6 +15,7 @@ app.use(express.json());
 app.get('/tiktok/download', async (req, res) => {
   const url = req.query.url; // Mengambil URL dari query parameter
 
+  // Memeriksa apakah URL disediakan
   if (!url) {
     return res.status(400).json({ error: 'URL tidak disediakan' });
   }
@@ -26,6 +27,14 @@ app.get('/tiktok/download', async (req, res) => {
     console.error('Error saat mengambil data:', error);
     res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
   }
+});
+
+// Menentukan port untuk server
+const PORT = process.env.PORT || 3000;
+
+// Menjalankan server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
 });
 
 // Ekspor handler untuk Vercel
