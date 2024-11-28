99e4f29010236a4144a327d455d1c7e476ffa1e2
wither3 <84785651+wither3@users.noreply.github.com>
2024-11-28 11:40:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const gemini = require('./codenya/gemini'); // Import modul Gemini
 // ./index.js
-const gemini = require('./codenya/gemini');
 const express = require('express');
 const cors = require('cors');
 const { downloadTikTok } = require('./codenya/ttdownload'); // Pastikan path ini sesuai dengan lokasi file ttdownload.js
+const { downloadVideo } = require('./codenya/tmate.js'); // Pastikan path ini sesuai dengan lokasi file tmate.js
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware untuk mengizinkan CORS
 app.use(cors());
@@ -16,7 +18,6 @@ app.use(express.json());
 app.get('/tiktok/download', async (req, res) => {
   const url = req.query.url; // Mengambil URL dari query parameter
 
-  // Memeriksa apakah URL disediakan
   if (!url) {
     return res.status(400).json({ error: 'URL tidak disediakan' });
   }
@@ -30,7 +31,7 @@ app.get('/tiktok/download', async (req, res) => {
   }
 });
 
-app.get('/gemini', async (req, res) => {
+app.get('/Gemini', async (req, res) => {
   const messages = req.query.messages; // Ambil parameter messages dari URL
   const temperature = parseFloat(req.query.temperature) || 0.9; // Parameter opsional
   const top_p = parseFloat(req.query.top_p) || 0.7; // Parameter opsional
@@ -63,6 +64,8 @@ app.get('/gemini', async (req, res) => {
   }
 });
 
+// Endpoint untuk mengunduh video menggunakan tmate
+// Endpoint untuk mengunduh video menggunakan tmate
 app.get('/tmate/download', async (req, res) => {
   const url = req.query.url; // Mengambil URL dari query parameter
 
@@ -80,21 +83,12 @@ app.get('/tmate/download', async (req, res) => {
     }
   } catch (error) {
     console.error('Terjadi kesalahan saat mendownload:', error);
-    return res.status(500).json({ error: 'Terjadi kesalahan saat mendownload.'});
+    return res.status(500).json({ error: 'Terjadi kesalahan saat mendownload.' });
   }
 });
 
-
-
-
-
-// Menentukan port untuk server
-const PORT = process.env.PORT || 3000;
 
 // Menjalankan server
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
-
-// Ekspor handler untuk Vercel
-module.exports = app;
